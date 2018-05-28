f01159d121e738094de00cc4516cc0f43ecc83d6
Taylor Williams <taylor@thewilliams.net>
2018-05-28 23:07:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+  attr_accessor :total, :discount, :items
 
+  def initialize (discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item (item, cost, quantity = 1)
+    @total += cost*quantity
+  end
+
+  def apply_discount
+    @total = @total - @discount
+  end
+end
